912b983d3992782b3dc907bcab346ce90c94bf1c
yash gupta <yashgupta1mole@gmail.com>
2023-06-20 19:21:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// if(1){
+//     let a=3;
+//     var b=23;
+//     console.log(a);
+// console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+if(true){
+    var x=34;
+    let y=3;
+    const z=43;
+}
+ console.log(x);
+//  console.log(y);
+
+ console.log(z);
+ const z=4;
+ clearTimeout()
